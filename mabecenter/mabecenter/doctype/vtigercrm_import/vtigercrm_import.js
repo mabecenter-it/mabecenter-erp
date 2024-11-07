b610966cf78527ee5b4c8f391f958185f43537ef
Jorge Devia <jorged94@mabecenter.org>
2024-11-07 23:48:39 +0000

    new doctype for import specific OV of vtigercrm

---
+++ mabecenter/mabecenter/doctype/vtigercrm_import/vtigercrm_import.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2024, Dante Devenir and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("VTigerCRM Import", {
+    refresh(frm) {
+
+    },
+    onload_post_render(frm) {
+		frm.trigger("update_primary_action");
+	},
+    update_primary_action(frm) {
+		if (frm.is_dirty()) {
+			frm.enable_save();
+			return;
+		}
+		frm.disable_save();
+		if (frm.doc.status !== "Success") {
+			if (!frm.is_new() && frm.has_import_file()) {
+				let label = frm.doc.status === "Pending" ? __("Start Import") : __("Retry");
+				frm.page.set_primary_action(label, () => frm.events.start_import(frm));
+			} else {
+				frm.page.set_primary_action(__("Save"), () => frm.save());
+			}
+		}
+	},
+});
